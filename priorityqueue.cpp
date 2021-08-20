780bee0753d17111740f82a732b9ac7e872bd6e4
P Paul Jonathan <70207656+ppauljonathan@users.noreply.github.com>
2021-08-20 21:03:11 +0530

    Update priorityqueue.cpp

--- priorityqueue.cpp
+++ priorityqueue.cpp
@@ -1,78 +1,83 @@
 #include<iostream>
 using namespace std;
 
-struct node{int d;node *p,*n;node(){p=NULL;n=NULL;}};
-
-void ex(node **a,node **b){int t;t=(*a)->d;(*a)->d=(*b)->d;(*b)->d=t;}
-
-class pq
-{
+template <typename T>
+class pq{
     private:
-    node *f,*b;
-    int hsize;
-    int len(){int i=0;node *t=f;while(t!=NULL){t=t->n;i++;}return i;}
+        T t;
+        int p;
+        bool isMin;
+        pq *f,*b,*next,*prev;
+        void nodex(pq *a,pq *b){
+            T t=a->t;
+            int p=a->p;
+
+            a->t=b->t;
+            a->p=b->p;
+
+            b->t=t;
+            b->p=p;
+        }
+        void build(){
+            pq *iter=f->next;
+            while(iter!=NULL){
+                if(isMin){
+                    if(iter->p<f->p){
+                        nodex(f,iter);
+                    }
+                }
+                else{
+                    if(iter->p>f->p){
+                        nodex(f,iter);
+                    }
+                }
+                iter=iter->next;
+            }
+        }
     public:
-    void build()
-    {
-        hsize=len();
-        if(hsize<=1){return;}
-        node *t=f;int max=f->d;
-        while(t!=NULL)
-        {
-            if(t->d>max){ex(&f,&t);break;}
-            t=t->n;
+        pq(bool isMin){
+            this->f=NULL;
+            this->b=NULL;
+            this->isMin=isMin;
         }
-    }
-    pq(){f=NULL;b=NULL;}
-    void enq()
-    {
-        int n;
-        cout<<"ENTER ELEMENT : ";cin>>n;
-        node *temp=new node;temp->d=n;
-        if(f==NULL&&b==NULL){f=temp;b=temp;return;}
-        temp->p=b;
-        b->n=temp;
-        b=temp;
-    }
-    void di()
-    {
-        if(f==NULL){cout<<"UNDERFLOW";return;}
-        node *t=f;
-        while(t){cout<<t->d<<" ";t=t->n;}
-    }
-    void peek(){if(f==NULL){cout<<"UNDERFLOW";return;}cout<<f->d<<" IS TOP ELEMENT"<<'\n';}
-    void extop()
-    {
-        node *temp;
-        if(f==NULL){b=NULL;cout<<"UNDERFLOW";return;}
-        temp=f;
-        cout<<temp->d<<" EXTRACTED";
-        f=f->n;
-        delete temp;
-    }
+        int len(){
+            if(f==NULL&&b==NULL){return 0;}
+            if(f==b){return 1;}
+            pq *iter=f;int len=0;
+            while(iter!=b){
+                len++;
+                iter=iter->next;
+            }
+        }
+        bool isEmpty(){
+            return (this->len()==0)?true:false;
+        }
+        void enq(T t,int p){
+            pq *k=new pq(isMin);
+            k->t=t;
+            k->p=p;
+            k->next=NULL;
+            if(f==NULL){f=k;b=k;return;}
+            b->next=k;
+            k->prev=b;
+            b=b->next;
+            build();
+        }
+        void disp(){
+            pq *iter=f;
+            while(iter!=NULL){
+                cout<<iter->t<<" "<<iter->p<<'\n';
+                iter=iter->next;
+            }
+        }
+        T peek(){
+            return &(f->t);
+        }
+
+        void deq(){
+            pq *temp=f;
+            f=f->next;
+            delete temp;
+            build();
+        }
 };
-
-void message(){cout<<"priority queue operations: \n\tPress 1 to enque\n\tPress 2 to peek\n\tPress 3 to extract top : ";}
-
-int main()
-{
-    pq q;
-    int n;
-    char c='y';
-    while(c=='y'||c=='Y')
-    {
-        q.build();
-        message();
-        cin>>n;
-        switch(n)
-        {
-            case 1:q.enq();break;
-            case 2:q.peek();break;
-            case 3:q.extop();break;
-        }
-        q.build();
-        //q.di();
-        cout<<"\n\rdo you want to continue : ";cin>>c;
-    }
-    return 0;
-}
